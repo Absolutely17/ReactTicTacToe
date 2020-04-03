ee6c6613f3fec17e67bf78c079cf5f82806003ee
Absolutely <absolutely1756@gmail.com>
2020-04-03 18:22:09 +0700

    init

--- server.js
+++ server.js
@@ -10,7 +10,5 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 app.listen(port);
